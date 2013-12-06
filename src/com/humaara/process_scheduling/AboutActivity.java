48fb5d496cc4da58a85df7fcf08d16514b6e5802
sicsr-aakash <gayatri.venugopal@sicsr.ac.in>
2013-12-06 09:43:54 +0530

    Process scheduling algorithms

--- src/com/humaara/process_scheduling/AboutActivity.java
+++ src/com/humaara/process_scheduling/AboutActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class AboutActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
